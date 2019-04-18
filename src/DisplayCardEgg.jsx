5b00323836e5f0e3ec52736b7a65e9ee3aa73983
Amine BEN TALEB <amine.bentaleb@gmx.fr>
2019-04-18 21:37:09 +0200

    button gets random egg defined egg image as a button

--- src/DisplayCardEgg.jsx
+++ src/DisplayCardEgg.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Card, CardImg, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import { Card, CardImg, CardBody, CardTitle, CardSubtitle, Button } from "reactstrap";
+import "./DisplayCardEgg.css"
 
 class DisplayCardEgg extends Component {
   constructor(props) {
@@ -9,12 +10,16 @@ class DisplayCardEgg extends Component {
     };
   }
   render() {
+    let button;
+    this.props.eggButton ? (button = '') : (button = 'Content-toggle');
     return (
       <div>
-        <Card>
+        <Card className = {button}>
           <CardBody>
             <CardTitle>Name : {this.props.eggs.name}</CardTitle>
-            <CardImg style={{width:"25%"}} src={this.props.eggs.image} alt="Egg" />
+            <Button>
+            <CardImg style={{width:"25%"}} src={this.props.eggs.image} alt="Egg"  />
+            </Button>
             <CardSubtitle>Rarity : {this.props.eggs.rarity}</CardSubtitle>
           </CardBody>
         </Card>
