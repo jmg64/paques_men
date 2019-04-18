b2983ee5585d6237355054c9b8ecf6745bd51c16
EnzoDuhalde <enzo-duhalde@hotmail.fr>
2019-04-18 22:25:56 +0200

    modified cards css

--- src/DisplayCardCharacter.jsx
+++ src/DisplayCardCharacter.jsx
@@ -2,28 +2,24 @@ import React, { Component } from "react";
 import { Card, CardImg, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 class DisplayCardCharacter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-     
-    };
-  }
-  render() {
-    return (
-      <div>
-        <Card>
-          <CardBody>
-            <CardTitle>Name : {this.props.character.name}</CardTitle>
-            <CardImg style={{width:"25%"}} src={this.props.character.image} alt="Egg" />
-            <CardSubtitle>Species : {this.props.character.species}</CardSubtitle>
-            <CardSubtitle>Origin : {this.props.character.origin}</CardSubtitle>
-          </CardBody>
-        </Card>
-      </div>
-      
-      
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="Cards">
+                <Card>
+                    <CardBody>
+                        <CardTitle>Name : {this.props.character.name}</CardTitle>
+                        <CardImg style={{ width: "25%" }} src={this.props.character.image} alt="Egg" />
+                        <CardSubtitle>Species : {this.props.character.species}</CardSubtitle>
+                        <CardSubtitle>Origin : {this.props.character.origin}</CardSubtitle>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
 }
 
 export default DisplayCardCharacter;
